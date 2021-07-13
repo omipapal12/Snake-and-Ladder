769a68f5ab23099594b51981b52e7673a23d074f
omipapal12 <omipapal12@gmail.com>
2021-07-13 23:39:26 +0530

    Welcome to the Snake and Ladder Game

---
+++ src/SnakeandLadder.java
@@ -0,0 +1,11 @@
+public class SnakeandLadder {
+	public static void main(String[] args) {
+		System.out.println("Welcome");
+		
+		int currentPosition =0;
+		System.out.println("Initial Position is "+currentPosition);
+	}
+
+
+}
+	
