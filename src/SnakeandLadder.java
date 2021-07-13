2e0ec9b5fff7eee43e6698fae9af39cde62c9966
omipapal12 <omipapal12@gmail.com>
2021-07-13 23:48:53 +0530

    Die no. between 1 to 6

--- src/SnakeandLadder.java
+++ src/SnakeandLadder.java
@@ -4,8 +4,9 @@ public class SnakeandLadder {
 		
 		int currentPosition =0;
 		System.out.println("Initial Position is "+currentPosition);
+
+		int die = (int) Math.floor(Math.random()*10)%6+1;
+		System.out.println("die is "+die);
 	}
-
-
 }
 	
