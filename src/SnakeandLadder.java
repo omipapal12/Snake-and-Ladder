ad1908cab4a32f6719c28e0a8f2649d3408f50e9
omipapal12 <omipapal12@gmail.com>
2021-07-13 23:53:28 +0530

    Checks for option

--- src/SnakeandLadder.java
+++ src/SnakeandLadder.java
@@ -1,4 +1,8 @@
 public class SnakeandLadder {
+	static final int noPlay = 0;
+	static final int ladder = 1;
+	static final int snake = 2;
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome");
 		
@@ -7,6 +11,30 @@ public class SnakeandLadder {
 
 		int die = (int) Math.floor(Math.random()*10)%6+1;
 		System.out.println("die is "+die);
+	
+int option = (int) Math.floor(Math.random()*10)%3;
+		
+
+		
+		switch(option)
+		{
+		case noPlay:
+			System.out.println("No change- Position is = "+currentPosition);
+			
+			break;
+		case ladder:
+			currentPosition = currentPosition + die;
+			System.out.println("Goes Ahead by " + die +" Position is = "+currentPosition);
+			
+			break;
+		case snake:
+			currentPosition = currentPosition - die;
+			System.out.println("Goes Behind by "+ die + " Position is = "+currentPosition);
+			
+			break;
+		default:
+			break;
+		}
 	}
 }
 	
