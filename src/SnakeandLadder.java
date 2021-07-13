17978da2f0806972a614695ced1d0bf5541bd51c
omipapal12 <omipapal12@gmail.com>
2021-07-14 00:09:13 +0530

    2 Player Game

--- src/SnakeandLadder.java
+++ src/SnakeandLadder.java
@@ -7,18 +7,20 @@ public class SnakeandLadder {
 		System.out.println("Welcome");
 		
 		int i=0;
+		int p=1;
 		
 		int count=0;
-		int currentPosition =0;
-		System.out.println("Initial Position is "+currentPosition);
+		int currentPosition1 =0;
+		int currentPosition2 =0;
+		System.out.println("Initial Position is "+currentPosition1);
 		
 		int die;
 		
 		int option;
 		
+		while(i==0) {
 		
-		
-		while(currentPosition <= 100 && i==0) {
+		while(currentPosition1 <= 100 && i==0 && p==1) {
 			
 			die = (int) Math.floor(Math.random()*10)%6+1;
 			System.out.println("die is "+die);
@@ -31,34 +33,81 @@ public class SnakeandLadder {
 		switch(option)
 		{
 		case noPlay:
-			System.out.println("No change- Position is = "+currentPosition);
-			
+			System.out.println("{Player1} No change- Position is = "+currentPosition1);
+			p = 2;
 			break;
 		case ladder:
-			currentPosition = currentPosition + die;
-			System.out.println("(Ladder) Goes Ahead by " + die +" Position is = "+currentPosition);
+			currentPosition1 = currentPosition1 + die;
+			System.out.println("{Player1}(Ladder) Goes Ahead by " + die +" Position is = "+currentPosition1);
 			
-			if(currentPosition>=100) {
-				if(currentPosition > 100) {
-					currentPosition = currentPosition - die;
+			if(currentPosition1>=100) {
+				if(currentPosition1 > 100) {
+					currentPosition1 = currentPosition1 - die;
 				}
 				else 
 				{
-					System.out.println("*****\nwinning psition = "+currentPosition+"\n********");
+					System.out.println("\n\n **  {Player1 WON}  *****\nwinning psition = "+currentPosition1+"\n********");
 					i=1;
 				}
 				}
 			
 			break;
 		case snake:
-			currentPosition = currentPosition - die;
-			if(currentPosition < 0) {
-			currentPosition=0;
+			currentPosition1 = currentPosition1 - die;
+			if(currentPosition1 < 0) {
+			currentPosition1=0;
 			}
-			System.out.println("(Snake) Goes Behind by "+ die + " Position is = "+currentPosition);
+			System.out.println("{Player1}(Snake) Goes Behind by "+ die + " Position is = "+currentPosition1);
+			p = 2;
 			break;
 		default:
 			break;
+		}
+		}
+		
+while(currentPosition2 <= 100 && i==0 && p==2) {
+			
+			die = (int) Math.floor(Math.random()*10)%6+1;
+			System.out.println("die is "+die);
+			count++;
+			
+			
+			
+			option = (int) Math.floor(Math.random()*10)%3;
+			
+		switch(option)
+		{
+		case noPlay:
+			System.out.println("{Player1}No change- Position is = "+currentPosition2);
+			p = 1;
+			break;
+		case ladder:
+			currentPosition2 = currentPosition2 + die;
+			System.out.println("{Player1}(Ladder) Goes Ahead by " + die +" Position is = "+currentPosition2);
+			
+			if(currentPosition2>=100) {
+				if(currentPosition2 > 100) {
+					currentPosition2 = currentPosition2 - die;
+				}
+				else 
+				{
+					System.out.println("\n\n **  {Player2 WON}  *****\nwinning psition = "+currentPosition2+"\n********");
+					i=1;
+				}
+				}
+			
+			break;
+		case snake:
+			currentPosition2 = currentPosition2 - die;
+			if(currentPosition2 < 0) {
+			currentPosition2=0;
+			}
+			System.out.println("{Player1}(Snake) Goes Behind by "+ die + " Position is = "+currentPosition2);
+			p = 1;
+			break;
+		default:
+			break;
+		}
 		}
 		}
 		
